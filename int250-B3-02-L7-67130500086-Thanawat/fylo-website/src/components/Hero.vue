c3464325ac34cf57352f2907d29a40cef4e806d1
thanawat li <thanawat@example.com>
2026-05-18 17:41:33 +0700

    add fylo

---
+++ int250-B3-02-L7-67130500086-Thanawat/fylo-website/src/components/Hero.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+    <section class="space-y-10 py-20">
+        <div class="flex justify-center ">
+            <img src="../assets/illustration-intro.png" alt="">
+        </div>
+        <div class="w-xl mx-auto space-y-10">
+        <div class=" w-sm text-center lg:w-2xl  text-3xl font-bold lg:text-left  md:mx-auto ">
+            <h1 >All your files in one secure location, accessible anywhere.</h1>
+        </div>
+        <div class=" w-sm text-center  lg:w-2xl lg:text-left md:mx-auto">
+            <h1>Fylo stores all your most important files in one secure location.
+                Access them wherever you need, share and collaborate with friends,
+                family, and co-workers.</h1>
+        </div>
+        </div>
+
+        <div class="flex justify-center">
+            <button class="rounded-full  bg-indigo-300 w-48 p-2 dark:text-white font-bold">Get Started</button>
+        </div>
+    </section>
+</template>
+
+<script setup></script>
